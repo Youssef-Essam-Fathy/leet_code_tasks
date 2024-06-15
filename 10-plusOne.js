478adc04808f4076c34f3d907a4fbf54e3796e0e
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-15 13:13:44 +0300

    solved

--- 10-plusOne.js
+++ 10-plusOne.js
@@ -2,10 +2,6 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-/**
- * @param {number[]} digits
- * @return {number[]}
- */
 var plusOne = function(digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] < 9) {
