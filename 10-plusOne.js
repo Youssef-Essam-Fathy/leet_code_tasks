bcf53050f2ecc318c7d8bb345e05d94ae7d80966
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-15 13:11:01 +0300

    solved

---
+++ 10-plusOne.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+};
+
+let digits = [9];
+console.log(plusOne(digits))
